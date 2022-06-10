31c098ee9e14faa8da8c09a43ee4e8e69b3e9b81
Wyatt LeFevre <wyatt@neighbor.com>
2022-06-10 14:28:47 -0600

    add a couple themes and set system76 as default for now

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -49,6 +49,7 @@ return packer.startup(function(use)
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "morhetz/gruvbox"
   use "folke/tokyonight.nvim"
+  use "joshdick/onedark.vim"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
