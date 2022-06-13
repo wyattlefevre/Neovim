f23f08aaa8139f103a168735fb1ba0c75c7143cf
Wyatt LeFevre <wyatt@neighbor.com>
2022-06-13 12:13:01 -0600

    faster vertical movement

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -37,9 +37,12 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
+-- Move quickly up and down
+keymap("n", "J", "5j", opts)
+keymap("n", "K", "5k", opts)
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("n", "∆", "<Esc>:m .+1<CR>==", opts)
+keymap("n", "˚", "<Esc>:m .-2<CR>==", opts)
 
 -- Clear highlight with esc 
 keymap("n", "<ESC>", ":noh<CR>", opts)
@@ -53,16 +56,16 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "∆", ":m .+1<CR>==", opts) -- configured for MacOS
+keymap("v", "˚", ":m .-2<CR>==", opts) -- configured for MacOS
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "∆", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "˚", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
