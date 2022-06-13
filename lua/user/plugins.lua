033069741f5b2fd3dc93a9ee14181badf74b9ea9
Wyatt LeFevre <wyatt@neighbor.com>
2022-06-13 11:54:54 -0600

    add commenting

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,7 +44,8 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-  use "windwp/nvim-autopairs"
+  use "windwp/nvim-autopairs" -- Autopairs, integrates with cmp and treesitter
+  use "numToStr/Comment.nvim" -- Easily comment thigns
 
   -- Colorschemes
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
@@ -82,6 +83,7 @@ return packer.startup(function(use)
   }
   use "p00f/nvim-ts-rainbow"
   -- use "nvim-treesitter/playground"
+  use "JoosepAlviste/nvim-ts-context-commentstring"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
