3d57a01d714e07aa939cbfd0daa6d4e1ded55312
Wyatt LeFevre <wyatt@neighbor.com>
2022-06-13 13:18:56 -0600

    add gitsigns

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -6,3 +6,4 @@ if not status_ok then
   return
 end
 vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
+vim.cmd([[hi CursorLine ctermbg=0 guibg=#000000]]) -- sets highlight line to black. makes it easier to read hint text
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,7 +45,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with cmp and treesitter
-  use "numToStr/Comment.nvim" -- Easily comment thigns
+  use "numToStr/Comment.nvim" -- Easily comment things
 
   -- Colorschemes
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
@@ -84,6 +84,10 @@ return packer.startup(function(use)
   use "p00f/nvim-ts-rainbow"
   -- use "nvim-treesitter/playground"
   use "JoosepAlviste/nvim-ts-context-commentstring"
+
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
